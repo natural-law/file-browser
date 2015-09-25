4fac7014d9a9de8696c955f0e0aa469bd938800a
zhangbin <zhangbin@cocos2d-x.org>
2015-09-25 11:34:03 +0800

    Init the package.

---
+++ main.js
@@ -0,0 +1,11 @@
+module.exports = {
+    load: function () {
+    },
+
+    unload: function () {
+    },
+
+    'file-browser:open': function () {
+        Editor.Panel.open('file-browser.panel');
+    }
+};
---
+++ panel/file-browser.js
@@ -0,0 +1,7 @@
+
+Editor.registerPanel( 'file-browser.panel', {
+    is : 'file-browser',
+    'file-browser:open-folder' : function( folder_path ) {
+
+    }
+});
